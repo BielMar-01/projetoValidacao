9ceb9f0eddcbcbdcc9d8d869eaa637f27c4fa6e9
Gabriel Marques <bielmarques922@gmail.com>
2020-01-24 11:30:10 -0300

    Ambiente

---
+++ resources/views/novocliente.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Cadastro de Clientes</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
---
+++ storage/framework/views/1dcf5e83daee14f3a3676ed68abee54ce192674d.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title>Cadastro de Clientes</title>
+
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+</head>
+<body>
+    <script src="<?php echo e(asset('js/app.js')); ?>" type="text/javascript"></script>
+</body>
+</html><?php /**PATH /home/gabriel/code/validacao/resources/views/novocliente.blade.php ENDPATH**/ ?>
